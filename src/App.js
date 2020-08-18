989849bcf879bcef7b3810778265dce0fe04363b
mhmulford0 <mhmulford0@gmail.com>
2020-08-17 19:16:50 -0500

    warning fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import Article from './components/Article'
 
