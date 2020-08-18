571b797b40399a9b0ed065de6d5e1cd047a6276b
mhmulford0 <mhmulford0@gmail.com>
2020-08-17 19:27:54 -0500

    started styles

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledArticle = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  width: 30%;
+  img {
+    width: 350px;
+  }
+  h1 {
+    font-size: 2.2rem;
+  }
+`;
 
 function Article(props) {
   const { author, title, description, url, urlToImage } = props.info;
   console.log(props)
   return (
-    <div>
+    <StyledArticle>
       <h1>{title}</h1>
       <img src={urlToImage} alt="" />
       <h2>{author}</h2>
       <p>{description}</p>
       <a href={url}>Full Article</a>
-    </div>
+    </StyledArticle>
   )
 }
 
